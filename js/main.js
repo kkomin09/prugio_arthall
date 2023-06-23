ab1e2afde73fa63b16f29fa2a3d5c707adaf4fa6
kkomin09 <kkomin09@naver.com>
2023-06-23 11:32:12 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// 스크린 높이값 확인하는거
+// let winW = $(window).height();
+// console.log(winW);
+
+//dom구조의 main 요소를 변수 mainEle에 담는다. 
+// let mainEle = $('main'); 
+// console.log(mainEle);
+
+// dom요소 main에 높이값을 원도우 높이값으로 대체한다.
+// mainEle.height(winW);
+
+
+
+// 꼭 알아야 하는 것들(jQuery)
+// scrollTop()  -> 스크롤한거 얼마인지
+// scrollTo()  -> 스크롤 이동
+// width() -> 요소의 넓이값 지정
+// height()
+// addClass()
+// removeClass()
+// find()
+// next()
+// children()
+// parent()
+// parentNode()
+
+// 효과
+// fadeIn()
+// fadeOut()
+// show()
+// hide()
+// scrollDown()
+// scrollUp()
+
+// 반복
+// setInterval
+
+// if
+// for
+
+// 선택자
+// $()
